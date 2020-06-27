76adf1e9b230d6b306bb38edfb232c05802c5efc
hsawalka <hamza.sawalka@gmail.com>
2020-06-27 15:19:19 +0300

    Improved coding style

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,9 +1,7 @@
 
-loginApp.controller('loginCtrl',['$scope','$http',($scope,$http) => {
-
-    $scope.login= () =>
-    {
-        $http.post('/api/login', {email:$scope.email , password:$scope.password}).then(r => {
+loginApp.controller('loginCtrl', ['$scope', '$http', ($scope, $http) => {
+    $scope.login= () => {
+        $http.post('/api/login', { email: $scope.email , password: $scope.password}).then(r => {
             $scope.user = r.data;
         }, e => {
             $scope.errorMessage = e.data.message;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // log all api traffic to console
-app.use('api/*',req=>{
+app.use('api/*', req => {
     console.log(req);
     next();
 });
@@ -22,33 +22,33 @@ Obviously email and password should come from a data base, but for now we're gon
 const email = '123@123.123';
 const password = '123123';
 
-app.post('/api/login', function (req, res) {
+app.post('/api/login', function(req, res) {
 
     if(req.body && req.body.email && req.body.password){
-        if(req.body.email == email){
-
+        if(req.body.email == email) {
             if(req.body.password == password) {
-                var user ={
-                    name:"Alex Jones"
-                    , email:req.body.email
-                    , password:req.body.password
-                    , profilePic:"http://lorempixel.com/500/500/people/"
+                var user = {
+                    name: 'Alex Jones',
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePic: 'http://lorempixel.com/500/500/people/',
                 };
                 res.send(200, user);
             }
-            else
-                res.send(400,{message:'hey lady, you sent me the wrong password.'});
-
-        }else
-            res.send(400,{message:'hey man, you sent me the wrong email.'});
-
+            else {
+                res.send(400, { message:'hey lady, you sent me the wrong password.' });
+            }
+        } else {
+            res.send(400, { message:'hey man, you sent me the wrong email.' });
+        }
     }
-    else
+    else {
         res.send(422,{message:'yo! you miss`n some stuff!'});
+    }
 });
 
 
-var serve = staticFiles('public/', {'index': ['index.html']});
+var serve = staticFiles('public/', { 'index': ['index.html'] });
 app.use(serve);
 
 
