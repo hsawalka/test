3bcd62279210208c336827faff69d67f9d6f0421
hsawalka <hamza.sawalka@gmail.com>
2020-06-27 15:30:12 +0300

    Made var names clearer

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,10 +1,10 @@
 
 loginApp.controller('loginCtrl', ['$scope', '$http', ($scope, $http) => {
     $scope.login= () => {
-        $http.post('/api/login', { email: $scope.email , password: $scope.password}).then(r => {
-            $scope.user = r.data;
-        }, e => {
-            $scope.errorMessage = e.data.message;
+        $http.post('/api/login', { email: $scope.email , password: $scope.password}).then(res => {
+            $scope.user = res.data;
+        }, err => {
+            $scope.errorMessage = err.data.message;
         });
     }
 }]);
