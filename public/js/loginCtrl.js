ab8a04b1395952fae427dc21c750dcec9c72195b
hsawalka <hamza.sawalka@gmail.com>
2020-06-27 16:12:57 +0300

    Added changePass function

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,8 +1,18 @@
 
 loginApp.controller('loginCtrl', ['$scope', '$http', ($scope, $http) => {
-    $scope.login= () => {
-        $http.post('/api/login', { email: $scope.email , password: $scope.password}).then(res => {
+    $scope.login = () => {
+        $http.post('/api/login', { email: $scope.email , password: $scope.password }).then(res => {
             $scope.user = res.data;
+        }, err => {
+            $scope.errorMessage = err.data.message;
+        });
+    }
+
+    $scope.changePass = () => {
+        $http.post('/api/change-pass', { password: $scope.newPassword }).then(res => {
+            $scope.user = res.data;
+            $scope.newPasswordMode = false;
+            $scope.passwordChanged = true;
         }, err => {
             $scope.errorMessage = err.data.message;
         });
